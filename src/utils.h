36c232d05a47dc5e535740c7082cd17157cc9db0
Matin <mghavami@berkeley.edu>
2021-05-02 21:19:19 -0700

    add incomplete gamma functions and the beta function to utils

--- src/utils.h
+++ src/utils.h
@@ -4,3 +4,11 @@
 void vec_add(double *dst, double *u, double *v, size_t k);
 
 void normalize(double *v, size_t n);
+
+inline double square(double x) { return x * x; }
+
+double ligamma(double s, double x);
+
+double uigamma(double s, double x) { return gamma(s) - ligamma(s, x); }
+
+double beta(double *x, size_t n);
